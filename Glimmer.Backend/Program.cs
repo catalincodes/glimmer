e832e36ccf215c852ed66a2a1bb43babd9ca9d54
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2026-08-09 14:39:34 +0200

    Stream words from a /chat endpoint

--- Glimmer.Backend/Program.cs
+++ Glimmer.Backend/Program.cs
@@ -6,24 +6,37 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapGet("/stream", (CancellationToken cancellationToken) => StreamWords(cancellationToken));
+app.MapGet("/stream", (CancellationToken cancellationToken) => StreamWords(null, cancellationToken));
 
-app.MapPost("/chat", (ChatRequest request) =>
-        {
-            return Results.Ok(new { echoed = request.Message });
-        });
-
-static async IAsyncEnumerable<string> StreamWords([EnumeratorCancellation]CancellationToken cancellationToken)
+app.MapPost("/chat", async (ChatRequest request, HttpContext context, CancellationToken token) =>
 {
-    var words = new[] { "Hello", " from", " Glimmer's", " asynchronous", " streaming", " backend!" };
+    context.Response.ContentType = "text/plain; charset=utf-8";
 
-    foreach (var word in words)
+    string[] words = request.Message.Split(' ');
+    var wordsList = words.ToList();
+
+    for (int i = 0; i < wordsList.Count; i++)
     {
+        var wordToStream = i < wordsList.Count - 1 ? wordsList[i] + " " : wordsList[i];
+ 
+        await Task.Delay(200, token);
+        await context.Response.WriteAsync(wordToStream, token);
+        await context.Response.Body.FlushAsync(token);
+    }
+});
+
+static async IAsyncEnumerable<string> StreamWords(IEnumerable<string>? wordsToStream, [EnumeratorCancellation]CancellationToken cancellationToken)
+{
+    var words = wordsToStream ?? [ "Hello", "from", "Glimmer's", "asynchronous", "streaming", "backend!" ];
+
+    var wordsList = words.ToList();
+    for (int i = 0; i < wordsList.Count; i++)
+    {
+        var wordToStream = i < wordsList.Count - 1 ? wordsList[i] + " " : wordsList[i];
         await Task.Delay(200, cancellationToken);
-        yield return word;
+        yield return wordToStream;
     }
 }
-
 
 app.Run();
 
